6a4029d16935bdd0bbe0f6f6721cbd12075ab7cd
siddhanth78 <siddhanth.lalgowdar@gmail.com>
2025-11-07 06:30:30 -0700

    done with grids

---
+++ game.py
@@ -0,0 +1,160 @@
+import curses
+import sys
+import json
+import math
+
+def inventory(stdscr, inv):
+    stdscr.clear()
+    c = 0
+    inv_len = len(inv)
+    for i in range(inv_len):
+        if i == c:
+            inv_text = "> "+inv[i]
+        else:
+            inv_text = inv[i]
+        stdscr.addstr(i, 0, inv_text)
+    stdscr.addstr(inv_len+1, 0, "j/k:scroll")
+    while True:
+        key = stdscr.getch()
+
+        # Navigate
+        if key == ord('j'):
+            c -= 1
+            if c < 0: c = 0
+        elif key == ord('k'):
+            c += 1
+            if c > inv_len-1: c = inv_len-1
+       
+        # Exit inventory
+        elif key == ord('i'):
+            stdscr.clear()
+            break
+        
+        stdscr.clear()
+        for i in range(inv_len):
+            if i == c:
+                inv_text = "> "+inv[i]
+            else:
+                inv_text = inv[i]
+            stdscr.addstr(i, 0, inv_text)
+        stdscr.addstr(inv_len+1, 0, "j/k:scroll | i:exit")
+    pass
+
+def change_grid(deltax, deltay, x, y, prevx, prevy, grid_id, grid, gamefile):
+    prevgridsize = [len(gamefile["grids"][grid_id][0]), len(gamefile["grids"][grid_id])]
+
+    if (int(grid_id)%5 == 1 and deltax < 0) or (int(grid_id)%5 == 0 and deltax > 0):
+        return x, y, prevx, prevy, grid_id, grid, prevgridsize, gamefile
+    if (int(grid_id) in [1,2,3,4,5] and deltay < 0) or (int(grid_id) in [16,17,18,19,20] and deltay > 0):
+        return x, y, prevx, prevy, grid_id, grid, prevgridsize, gamefile
+ 
+    grid[y][x] = ' '
+    gamefile["grids"][grid_id] = grid
+    grid_id = str(int(grid_id)+deltax+(deltay*5))
+    grid = gamefile["grids"][grid_id]
+    grid_size = [len(grid[0]), len(grid)]
+
+    prevx, prevy = x, y
+
+    if deltax > 0:
+        x = 0
+    elif deltax < 0:
+        x = grid_size[0]-1
+    elif deltay > 0:
+        y = 0
+    elif deltay < 0:
+        y = grid_size[1]-1
+    
+    if prevx > grid_size[0]-1 and deltay != 0:
+        x = grid_size[0]-1
+    if prevy > grid_size[1]-1 and deltax != 0:
+        y = grid_size[1]-1
+    prevx, prevy = -1, -1
+    return x, y, prevx, prevy, grid_id, grid, grid_size, gamefile
+
+def update_screen(stdscr, x, y, prevx, prevy, grid, grid_size, grid_id, cleared=False):
+    if cleared == True:
+        stdscr.clear()
+    if prevx >= 0 and prevy >= 0:
+        grid[prevy][prevx] = ' '
+    grid[y][x] = '0'
+    for i in range(grid_size[1]):
+        stdscr.addstr(i+1,0,"|")
+        stdscr.addstr(i+1,grid_size[0]+1,"|")
+        for j in range(grid_size[0]):
+            stdscr.addstr(0,j+1,"-")
+            stdscr.addstr(grid_size[1]+1,j+1,"-")
+            stdscr.addstr(i+1,j+1,grid[i][j])
+    stdscr.addstr(grid_size[1]+3,0,f"Grid: {((int(grid_id)-1)%5)+1}, {math.floor((int(grid_id)-1)/5)+1}")
+    stdscr.addstr(grid_size[1]+4,0,"wasd:move | q:quit | i:inventory")
+    stdscr.refresh()
+
+def main(stdscr):
+
+    with open("grids.json", "r") as file:
+        gamefile = json.load(file)
+
+    grid_id = gamefile["curr_grid"]
+    grid_size = [len(gamefile["grids"][grid_id][0]), len(gamefile["grids"][grid_id])]
+    grid = gamefile["grids"][grid_id]
+    inv = gamefile["inventory"]
+
+    curses.noecho()
+    curses.cbreak()
+    curses.curs_set(0)
+    stdscr.keypad(True)
+    x,y = gamefile["player"]
+    prevx, prevy = -1,-1
+    update_screen(stdscr, x, y, prevx, prevy, grid, grid_size, grid_id)
+
+    while True:
+        grid_swap = False
+        key = stdscr.getch()
+        
+        # Movement
+        if key == ord('w'):
+            if y > 0:
+                prevx, prevy = x, y
+                y -= 1
+            elif y <= 0:
+                x, y, prevx, prevy, grid_id, grid, grid_size, gamefile = change_grid(0, -1, x, y, prevx, prevy, grid_id, grid, gamefile)
+                grid_swap = True
+        elif key == ord('s'):
+            if y < grid_size[1]-1:
+                prevx, prevy = x, y
+                y += 1
+            elif y >= grid_size[1]-1:
+                x, y, prevx, prevy, grid_id, grid, grid_size, gamefile = change_grid(0, 1, x, y, prevx, prevy, grid_id, grid, gamefile)
+                grid_swap = True
+        elif key == ord('a'):
+            if x > 0:
+                prevx, prevy =x, y
+                x -= 1
+            elif x <= 0:
+                x, y, prevx, prevy, grid_id, grid, grid_size, gamefile = change_grid(-1, 0, x, y, prevx, prevy, grid_id, grid, gamefile)
+                grid_swap = True
+        elif key == ord('d'):
+            if x < grid_size[0]-1:
+                prevx, prevy = x, y
+                x += 1
+            elif x >= grid_size[0]-1:
+                x, y, prevx, prevy, grid_id, grid, grid_size, gamefile = change_grid(1, 0, x, y, prevx, prevy, grid_id, grid, gamefile)
+                grid_swap = True
+
+        # Inventory Menu
+        elif key == ord('i'):
+            inventory(stdscr, inv)
+
+        # Exit
+        elif key == ord('q'):
+            gamefile["grids"][grid_id] = grid
+            gamefile["curr_grid"] = grid_id
+            gamefile["player"] = [x,y]
+            with open("grids.json", "w") as f:
+                json.dump(gamefile, f)
+            sys.exit(0)
+        
+        
+        update_screen(stdscr, x, y, prevx, prevy, grid, grid_size, grid_id, cleared=grid_swap)
+
+curses.wrapper(main)
---
+++ generate_grids.py
@@ -0,0 +1,20 @@
+import json
+import random
+
+def generate():
+    gjson = dict()
+    for i in range(1, 21):
+        grid_size = (random.randint(10, 50),random.randint(10, 25))
+        grid = [[' ' for _ in range(grid_size[0])] for _ in range(grid_size[1])]
+
+        gjson[str(i)] = grid
+
+    wjson = {"grids":gjson, "curr_grid":"1", "player": [0,0], "inventory": ["Sword", "Shield", "Potion"]}
+
+    with open("grids.json", "w") as f:
+        json.dump(wjson, f, indent=2)
+
+    print("Generated world")
+
+if __name__ == "__main__":
+    generate()
